7031c4d86e56b15b38af56ec2c119b9a8fa0ff2b
Josh McCulloch <mcculloch@windowslive.com>
2016-07-17 18:39:54 +1200

    Completed commenting of code and made small refators to make things clearer.

--- C/Forking/interprocess_communication.c
+++ C/Forking/interprocess_communication.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <errno.h>
 
+/* Definitions of the possible math operations that can be passed
+ * between processes
+ */
 typedef enum {add, subtract, multiply, divide} MathOperations;
 
+/* Definition of the interprocess message structure
+ */
 typedef struct {
 	MathOperations operation;
 	double operand1;
@@ -13,26 +18,66 @@ typedef struct {
 } MathOp;
 
 int main(void) {
+	/* We create our two interprocess pipes for comunication. The first 
+	 * is the parent to child (ptc) and the child to parent (ctp). These
+	 * pipes are defined as arrays of two integers, this is because when
+	 * we create a pipe we are given two file handes, one for writing 
+	 * and the other for reading.
+	 */
 	int ptcPipe[2];
 	int ctpPipe[2];
 	
+	/* We create our pipes and check for errors
+	 */
 	if (pipe(ptcPipe)) {
 		printf("Error creating parent to child pipe: %d", errno);
+		exit(1);
 	}
 	
 	if (pipe(ctpPipe)) {
 		printf("Error creating child to parent pipe: %d", errno);
+		exit(2);
 	}
 	
+	/* We fork the application, from this point forward there are two
+	 * seperate processes running. When for is called it returns the 
+	 * childs process id to the parent and 0 to the child. This allows
+	 * us to seperate the two.
+	 */
 	if (fork() == 0 ) {
-		
-		printf("I am the child\n");
+		/* Only the child process will execute this code. We close the 
+		 * ptcPipe's write file handle and the ctpPipe's read file
+		 * handle.
+		 */
 		close(ptcPipe[1]);
 		close(ctpPipe[0]);
+		
+		/* We create a buffer for storing incomming MathOp messages.
+		 */
 		char buffer[sizeof(MathOp)];
-		read(ptcPipe[0], buffer, sizeof(MathOp));
+		
+		/* We create a MathOp pointer and point it at the buffer we
+		 * created. This will allow us to read values out of the buffer
+		 * by using the stucts members.
+		 */
 		MathOp *request = (MathOp*)buffer;
-		printf("Operand %f and %f\n", request->operand1, request->operand2);
+		
+		/* We now call read a message from the ptcPipe into the buffer 
+		 * we created. This will block execution until a message is
+		 * ready.
+		 */
+		printf("Child: Ready for MathOp request\n");
+		read(ptcPipe[0], buffer, sizeof(MathOp));
+		
+		/* Once the buffer has been populated the struct's members can
+		 * be accessed like normal.
+		 */
+		printf("Child: Recieved MathOp Request - Operands =(%f, %f)\n", 
+			request->operand1, request->operand2);
+		
+		/* We execute the correct math operation based on the operatio
+		 * member and store the result back into the same stuct.
+		 */
 		switch (request->operation) {
 			case add:
 				request->result = request->operand1 + request->operand2;
@@ -48,21 +93,45 @@ int main(void) {
 				break;
 		}
 		
+		/* Finally we write the contents of the buffer into the ctpPipe
+		 * which sends the result back to the parent process.
+		 */
 		write(ctpPipe[1], buffer, sizeof(MathOp));
 	}
 	else {
-		char buffer[sizeof(MathOp)];
-		printf("I am the parent\n");
+		/* Only the parent process will execute this code. We close the 
+		 * ptcPipe's read file handle and the ctpPipe's write file
+		 * handle.
+		 */
 		close(ptcPipe[0]);
 		close(ctpPipe[1]);
+		
+		/* Once again we create a buffer to store any recieved messages
+		 * and a point to allow easy access to each of the MathOp 
+		 * members.
+		 */
+		char buffer[sizeof(MathOp)];
+		MathOp *result = (MathOp*)buffer;
+		
+		/* We create a new MathOp request and populate its values.
+		 */
 		MathOp request;
 		request.operation = add;
 		request.operand1 = 10;
 		request.operand2 = 12;
+		
+		/* We sennd the request MathOp to the child by handing its
+		 * address to the write function.
+		 */
+		printf("Parent: Sending MathOp request\n");
 		write(ptcPipe[1], &request, sizeof(MathOp));
+		
+		/* And then we wait for a result to be returnd from the child.
+		 */
 		read(ctpPipe[0], buffer, sizeof(MathOp));
-		MathOp *result = (MathOp*)buffer;
-		printf("Result: %f", result->result);
+		
+		/* Lastly we print the result*/
+		printf("Parent: Result = %f", result->result);
 	}
 	return EXIT_SUCCESS;
 }
